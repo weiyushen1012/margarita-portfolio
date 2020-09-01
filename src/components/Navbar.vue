87978fbf8b32beb6caebf61af809f0a2e97322e8
Weiyu Shen <weiyushen1012@gmail.com>
2020-09-01 17:07:01 -0400

    add navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav class="navbar">
+    <div class="container">
+      <div class="navbar-brand">
+        <h1 class="navbar-item">
+          Margarita Chavez
+        </h1>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+
+export default {
+  name: 'Navbar',
+}
+</script>
+
+<style scoped>
+  h1 {
+    letter-spacing: 0.2em;
+    text-transform: uppercase;
+  }
+
+</style>
---
+++ src/components/Reel.vue
@@ -0,0 +1,50 @@
+<template>
+  <section class="hero is-primary is-medium">
+    <div class="container">
+      <div class="columns ">
+        <div class="column">
+          <div class="hero-body">
+            <div class="introduction container">
+              <h2 class="title">
+                Director, Editor, Screen Writer
+              </h2>
+              <br>
+              <p class="subtitle">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium commodi culpa ducimus eligendi
+                explicabo
+                harum laboriosam magni maiores maxime molestiae molestias, nisi nostrum obcaecati placeat provident quae
+                quis
+                quisquam repellendus reprehenderit, saepe sed sint sit soluta ullam veritatis voluptas voluptates?
+                Aperiam ex
+                excepturi facere in, officiis porro sed sunt voluptates.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="video-reel column">
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/KYSiuH8Ln4w" frameborder="0"
+                  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen></iframe>
+        </div>
+
+      </div>
+    </div>
+
+
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Reel',
+}
+</script>
+
+<style scoped>
+.video-reel {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
